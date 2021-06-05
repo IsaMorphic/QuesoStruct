bb7adde0928cff95c88e9d47d782e6eb2a07f288
Yodadude2003 <yodadude2003@gmail.com>
2021-06-05 09:35:38 -0500

    Licensing

--- QuesoStruct/Types/Primitives/Bytes.cs
+++ QuesoStruct/Types/Primitives/Bytes.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+    QuesoStruct makes .NET based binary serialization code neat and easy
+    Copyright (C) 2021 Chosen Few Software
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -55,7 +72,7 @@ namespace QuesoStruct.Types.Primitives
             Parent = context.Current;
         }
 
-        public Bytes() { References = new HashSet<IStructReference>(); }
+        public Bytes(IStructInstance parent = null) { Parent = parent; References = new HashSet<IStructReference>(); }
 
         public long? Offset { get; set; }
         public IStructInstance Parent { get; set; }
